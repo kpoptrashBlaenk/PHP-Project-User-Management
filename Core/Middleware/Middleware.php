49722047d1b7cde771be5a6ac11f211bbddd0ac2
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-01-30 15:11:27 +0100

    middleware class

---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Core\Middleware;
+
+class Middleware
+{
+    public const MAP = [
+    ];
+
+    public static function resolve(array|null $keys): void
+    {
+        if (!$keys) {
+            return;
+        }
+
+        $redirect = null;
+
+        foreach ($keys as $key) {
+            $middleware = static::MAP[$key] ?? false;
+
+            if (!$middleware) {
+                throw new \Exception("No matching middleware found for {$key}");
+            }
+
+            $redirect = (new $middleware)->handle();
+
+            if (!$redirect) {
+                break;
+            }
+        }
+
+        if ($redirect) {
+            redirect($redirect);
+        }
+    }
+}
