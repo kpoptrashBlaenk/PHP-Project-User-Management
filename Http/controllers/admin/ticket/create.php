38886d40eb2fad2b36006b9af5e4901e71f6a1ab
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-02-07 16:18:13 +0100

    create is done

--- Http/controllers/admin/ticket/create.php
+++ Http/controllers/admin/ticket/create.php
@@ -8,15 +8,14 @@ $old = isset($_SESSION['_flashed']) ? $_SESSION['_flashed']['old'] : [];
 $app = new App;
 $db = $app->getDB();
 
-$getCategoriesQuery =
-    "SELECT categorie.id_categorie AS category_id,
-            categorie.libelle_categorie AS category
-     FROM categorie";
+$getCardsQuery =
+    "SELECT usager.id_carte AS card_id
+     FROM usager";
 
-$categories = $db->query($getCategoriesQuery)->get();
+$cards = $db->query($getCardsQuery)->get();
 
-view('admin/card/create.view.php', [
-    'categories' => $categories,
+view('admin/ticket/create.view.php', [
+    'cards' => $cards,
     'errors' => $errors,
     'old' => $old
 ]);
--- views/admin/ticket/create.view.php
+++ views/admin/ticket/create.view.php
@@ -4,38 +4,24 @@ require basePath('views/partials/header.php');
 ?>
 
 <div class="mt-5 pt-5 container">
-    <form class="row g-3" action="/admin/card/store" method="POST">
+    <form class="row g-3" action="/admin/ticket/store" method="POST">
 
         <div class="col-md-3">
-            <label for="name_input" class="form-label">Name</label>
-            <input type="text" class="form-control <?= isset($errors['name']) ? 'is-invalid' : '' ?>" id="name_input"
-                name="name_input" value="<?= $old['name'] ?? '' ?>">
-            <div class="invalid-feedback"><?= $errors['name'] ?? '' ?></div>
-        </div>
-
-        <div class="col-md-3">
-            <label for="category_input" class="form-label">Category</label>
-            <select class="form-select <?= isset($errors['category']) ? 'is-invalid' : '' ?>" id="category_input"
-                name="category_input">
-                <option value="" selected>Choose a category...</option>
-                <?php foreach ($categories as $category): ?>
-                    <option value="<?= $category['category_id'] ?>"> <?= $category['category'] ?></option>
+            <label for="card_input" class="form-label">Card</label>
+            <select class="form-select <?= isset($errors['card']) ? 'is-invalid' : '' ?>" id="card_input"
+                name="card_input">
+                <option value="" selected>Choose a card...</option>
+                <?php foreach ($cards as $card): ?>
+                    <option value="<?= $card['card_id'] ?>"> <?= $card['card_id'] ?></option>
                 <?php endforeach; ?>
             </select>
-            <div class="invalid-feedback"><?= $errors['category'] ?? '' ?></div>
-        </div>
-
-        <div class="col-md-3">
-            <label for="caution_input" class="form-label">Caution</label>
-            <input type="number" class="form-control <?= isset($errors['caution']) ? 'is-invalid' : '' ?>"
-                id="caution_input" name="caution_input" min="0" max="100" value="<?= $old['caution'] ?? '' ?>">
-            <div class="invalid-feedback"><?= $errors['caution'] ?? '' ?></div>
+            <div class="invalid-feedback"><?= $errors['card'] ?? '' ?></div>
         </div>
 
         <div class="col-md-3">
             <label for="date_input" class="form-label">Date</label>
             <input type="date" class="form-control <?= isset($errors['date']) ? 'is-invalid' : '' ?>" id="date_input"
-                name="date_input" min="0" max="100" value="<?= $old['date'] ?? '' ?>">
+                name="date_input" value="<?= $old['date'] ?? '' ?>">
             <div class="invalid-feedback"><?= $errors['date'] ?? '' ?></div>
         </div>
 
