2f775b7676ec6758a6aab0227cbe1f81fc1a9469
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-02-07 15:13:52 +0100

    edit is done

--- Http/controllers/admin/card/edit.php
+++ Http/controllers/admin/card/edit.php
@@ -5,23 +5,35 @@ use Core\App;
 $errors = isset($_SESSION['_flashed']) ? $_SESSION['_flashed']['errors'] : [];
 $old = isset($_SESSION['_flashed']) ? $_SESSION['_flashed']['old'] : [];
 
-$category = $_GET['category_id'];
+$cardId = $_GET['card_id'];
 
 $app = new App;
 $db = $app->getDB();
 
-$getCategoryQuery =
-    "SELECT categorie.id_categorie AS category_id,
-            categorie.libelle_categorie AS category
-     FROM categorie
-     WHERE categorie.id_categorie = :category_id";
+// Get selected card
+$getCardQuery =
+    "SELECT usager.nom AS name,
+            usager.id_categorie AS category_id,
+            usager.montant_caution AS caution,
+            usager.date_carte AS date
+     FROM usager
+     WHERE usager.id_carte = :card_id";
 
-$category = $db->query($getCategoryQuery, [
-    'category_id' => $category,
+$card = $db->query($getCardQuery, [
+    'card_id' => $cardId,
 ])->find();
 
-view('admin/category/edit.view.php', [
-    'category' => $category,
+// Get categories
+$getCategoriesQuery =
+    "SELECT categorie.id_categorie AS category_id,
+            categorie.libelle_categorie AS category
+     FROM categorie";
+
+$categories = $db->query($getCategoriesQuery)->get();
+
+view('admin/card/edit.view.php', [
+    'card' => $card,
+    'categories' => $categories,
     'errors' => $errors,
     'old' => $old
 ]);
--- views/admin/card/edit.view.php
+++ views/admin/card/edit.view.php
@@ -4,16 +4,41 @@ require basePath('views/partials/header.php');
 ?>
 
 <div class="mt-5 pt-5 container">
-    <form class="row g-3" action="/admin/category/update" method="POST">
+    <form class="row g-3" action="/admin/card/update" method="POST">
         <input hidden name="_method" value="PATCH" id="method">
-        <input hidden name="category_id" value="<?= $category['category_id'] ?>">
+        <input hidden name="card_id" value="<?= $category['card_id'] ?>">
 
-        <div class="col-md-4">
+        <div class="col-md-3">
+            <label for="name_input" class="form-label">Name</label>
+            <input type="text" class="form-control <?= isset($errors['name']) ? 'is-invalid' : '' ?>" id="name_input"
+                name="name_input" min="0" max="100" value="<?= $old['name'] ?? $card['name'] ?>">
+            <div class="invalid-feedback"><?= $errors['name'] ?? $card['name'] ?></div>
+        </div>
+
+        <div class="col-md-3">
             <label for="category_input" class="form-label">Category</label>
-            <input type="text" class="form-control <?= isset($errors['category']) ? 'is-invalid' : '' ?>"
-                id="category_input" name="category_input" min="0" max="100"
-                value="<?= $old['category'] ?? $category['category'] ?>">
-            <div class="invalid-feedback"><?= $errors['category'] ?? $category['category'] ?></div>
+            <select class="form-select <?= isset($errors['category']) ? 'is-invalid' : '' ?>" id="category_input"
+                name="category_input">
+                <?php foreach ($categories as $category): ?>
+                    <option value="<?= $category['category_id'] ?>" <?= $category['category_id'] === $card['category_id'] ? 'selected' : '' ?>> <?= $category['category'] ?></option>
+                <?php endforeach; ?>
+            </select>
+            <div class="invalid-feedback"><?= $errors['category'] ?? '' ?></div>
+        </div>
+
+        <div class="col-md-3">
+            <label for="caution_input" class="form-label">Caution</label>
+            <input type="text" class="form-control <?= isset($errors['caution']) ? 'is-invalid' : '' ?>"
+                id="caution_input" name="caution_input" min="0" max="100"
+                value="<?= $old['caution'] ?? $card['caution'] ?>">
+            <div class="invalid-feedback"><?= $errors['caution'] ?? $card['caution'] ?></div>
+        </div>
+
+        <div class="col-md-3">
+            <label for="date_input" class="form-label">Caution</label>
+            <input type="date" class="form-control <?= isset($errors['date']) ? 'is-invalid' : '' ?>" id="date_input"
+                name="date_input" min="0" max="100" value="<?= $old['date'] ?? $card['date'] ?>">
+            <div class="invalid-feedback"><?= $errors['date'] ?? $card['date'] ?></div>
         </div>
 
         <div class="col-12">
